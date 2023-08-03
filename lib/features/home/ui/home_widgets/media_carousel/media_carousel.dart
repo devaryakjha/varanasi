4fd52c4f1cf0697aa48c3954a1af9ede45027812
devaryakumar <dev.jha.arya@gmail.com>
2023-08-04 02:50:58 +0530

    🚧 Completed home screen UI

---
+++ lib/features/home/ui/home_widgets/media_carousel/media_carousel.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:varanasi_mobile_app/models/media_playlist.dart';
+import 'package:varanasi_mobile_app/utils/extensions/extensions.dart';
+
+import 'media_card.dart';
+
+class MediaCarousel extends StatelessWidget {
+  final MediaPlaylist playlist;
+
+  const MediaCarousel({
+    super.key,
+    required this.playlist,
+  });
+
+  int get totalItems => playlist.mediaItems?.length ?? 0;
+  String get title => playlist.title ?? '';
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 220,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          if (title.isNotEmpty) ...[
+            Padding(
+              padding: const EdgeInsets.only(left: 8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    title,
+                    style: context.textTheme.titleLarge
+                        ?.copyWith(fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+          ],
+          const SizedBox(height: 16),
+          Expanded(
+            child: ListView.separated(
+              separatorBuilder: (context, index) => const SizedBox(width: 16),
+              scrollDirection: Axis.horizontal,
+              itemCount: totalItems,
+              itemBuilder: (context, index) {
+                final media = playlist.mediaItems![index];
+                return MediaCard(
+                  media: media,
+                  isFirst: index == 0,
+                  isLast: index == totalItems - 1,
+                );
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
