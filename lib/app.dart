648963f87884ad94521500a3f48bbafa28542170
devaryakjha <dev.jha.arya@gmail.com>
2023-10-17 00:24:23 +0530

    🚧 Halt: on authentication

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:varanasi_mobile_app/features/session/cubit/session_cubit.dart';
 import 'package:varanasi_mobile_app/utils/constants/constants.dart';
 import 'package:varanasi_mobile_app/utils/router.dart';
 import 'package:varanasi_mobile_app/widgets/responsive_sizer.dart';
@@ -20,10 +19,6 @@ class Varanasi extends StatelessWidget {
         builder: (context, orientation, screenType) {
           return MultiBlocProvider(
             providers: [
-              BlocProvider(
-                lazy: false,
-                create: (context) => SessionCubit()..init(),
-              ),
               BlocProvider(
                 lazy: false,
                 create: (context) => ConfigCubit()..init(),
