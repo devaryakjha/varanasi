144f1ab716a25423cb3460d37dd7b8fc2e42bfe0
Aryakumar Jha <dev.jha.arya@gmail.com>
2023-10-23 15:23:24 +0530

    ✨ Add: empty state for user library page (#39)
    * ✨ New: Added empty state
    
    * 👷 Updated: Version

---
+++ lib/features/user-library/ui/widgets/add_playlist_button.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddPlaylistButton extends StatelessWidget {
+  const AddPlaylistButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: () {},
+      icon: const Icon(Icons.add_rounded),
+    );
+  }
+}
---
+++ lib/features/user-library/ui/widgets/empty.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:varanasi_mobile_app/utils/extensions/extensions.dart';
+import 'package:varanasi_mobile_app/utils/extensions/media_query.dart';
+
+class EmptyUserLibrary extends StatelessWidget {
+  const EmptyUserLibrary({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: context.width,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            "Add music to your library",
+            style: context.textTheme.titleLarge?.copyWith(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 8),
+          Text(
+            "Collect your favorites so that you can listen\nwhenever you want",
+            style: context.textTheme.titleSmall,
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+}
