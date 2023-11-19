e141f741ab9b20f9c157e82adabbc9f6d94e4980
devaryakjha <dev.jha.arya@gmail.com>
2023-11-19 23:46:50 +0530

    Remove FloatingActionButton from HomePage

--- lib/features/home/ui/home_screen.dart
+++ lib/features/home/ui/home_screen.dart
@@ -33,9 +33,6 @@ class HomePage extends StatelessWidget {
     };
     final greeting = generateGreeting();
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => throw Exception('Test Crash'),
-      ),
       appBar: AppBar(
         centerTitle: false,
         title: Row(
