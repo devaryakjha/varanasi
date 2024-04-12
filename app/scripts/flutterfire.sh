e19267c07541f7cd8264d5cb8e0ed8b4438094eb
logiclynxAJ <dev.jha.arya@gmail.com>
2024-04-13 05:29:00 +0530

    Remove unused macOS files

---
+++ app/scripts/flutterfire.sh
@@ -0,0 +1,42 @@
+# Accept flavor as an argument
+flavor=$1
+
+# IF flavor is not provided, set default to prod
+if [ -z "$flavor" ]; then
+  echo "Flavor not provided, defaulting to main"
+  flavor="main"
+fi
+
+# If flavor is not one of [staging, dev, prod], exit
+if [ "$flavor" != "staging" ] && [ "$flavor" != "development" ] && [ "$flavor" != "main" ]; then
+  echo "Invalid flavor"
+  echo "Valid flavors: [staging, development, main]"
+  exit 1
+fi
+
+package_base="dev.aryak.varanasi"
+
+if [ "$flavor" != "main" ]; then 
+    out="lib/firebase_options.dart"
+else 
+    out="lib/firebase_options_$flavor.dart"
+fi
+
+if [ "$flavor" != "main" ]; then 
+    package_name="$package_base"
+else
+    if [ "$flavor" != "staging" ]; then 
+        append=".stg"
+    else
+        append=".dev"
+    fi
+    package_name="$package_base.$append"
+fi
+
+flutterfire config \
+  --platforms=android,ios \
+  --out=$out \
+  --ios-bundle-id=$package_name \
+  --ios-out=ios/config/$flavor/GoogleService-Info.plist \
+  --android-package-name=$package_name \
+  --android-out=android/app/src/$flavor/google-services.json
