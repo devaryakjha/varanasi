cf5d6acae47980af2cf337e91814b092caffa3dd
logiclynxAJ <dev.jha.arya@gmail.com>
2024-04-13 05:07:49 +0530

    re initialised with very_good_cli

---
+++ app/android/app/src/main/kotlin/dev/aryak/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.aryak.varanasi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ app/lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:varanasi/app/app.dart';
+import 'package:varanasi/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
