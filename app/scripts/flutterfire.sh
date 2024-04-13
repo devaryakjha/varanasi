1d2742bcf730efd5ef5f9fe5667aaff08507fd90
logiclynxAJ <dev.jha.arya@gmail.com>
2024-04-13 06:06:44 +0530

    Update FlutterFire configuration script to support Android and iOS platforms

--- app/scripts/flutterfire.sh
+++ app/scripts/flutterfire.sh
@@ -35,7 +35,7 @@ else
 fi
 
 flutterfire config \
-  --platforms=ios \
+  --platforms=android,ios \
   --out=$out \
   --ios-bundle-id=$package_name \
   --ios-out=ios/config/$flavor/GoogleService-Info.plist \
