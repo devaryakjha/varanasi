1a298cb313fd77f97c19eeb2eb59b6020bfbe3e0
logiclynxAJ <dev.jha.arya@gmail.com>
2024-04-15 07:03:24 +0530

    Refactor dashboard page UI, add random avatar support, and update navigation bar in dashboard_page.dart

--- app/lib/app/features/dashboard/presentation/pages/dashboard_page.dart
+++ app/lib/app/features/dashboard/presentation/pages/dashboard_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:varanasi/app/shared/shared.dart';
 
 class DashboardPage extends StatelessWidget {
   const DashboardPage({
@@ -15,40 +14,41 @@ class DashboardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const DashboardAppbar(),
-      body: children[navigationShell.currentIndex],
-      bottomNavigationBar: NavigationBar(
-        height: 60,
-        selectedIndex: navigationShell.currentIndex,
-        onDestinationSelected: (index) {
-          navigationShell.goBranch(
-            index,
-            initialLocation: index == navigationShell.currentIndex,
-          );
-        },
-        destinations: const [
-          NavigationDestination(
-            icon: Icon(Icons.home_outlined),
-            selectedIcon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.search),
-            selectedIcon: Icon(Icons.search),
-            label: 'Search',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.library_books_outlined),
-            selectedIcon: Icon(Icons.library_books),
-            label: 'Your Library',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.person_2_outlined),
-            selectedIcon: Icon(Icons.person_2),
-            label: 'Account',
-          ),
-        ],
+      body: SafeArea(
+        child: children[navigationShell.currentIndex],
       ),
+      // bottomNavigationBar: NavigationBar(
+      //   height: 60,
+      //   selectedIndex: navigationShell.currentIndex,
+      //   onDestinationSelected: (index) {
+      //     navigationShell.goBranch(
+      //       index,
+      //       initialLocation: index == navigationShell.currentIndex,
+      //     );
+      //   },
+      //   destinations: const [
+      //     NavigationDestination(
+      //       icon: Icon(Icons.home_outlined),
+      //       selectedIcon: Icon(Icons.home),
+      //       label: 'Home',
+      //     ),
+      //     NavigationDestination(
+      //       icon: Icon(Icons.search),
+      //       selectedIcon: Icon(Icons.search),
+      //       label: 'Search',
+      //     ),
+      //     NavigationDestination(
+      //       icon: Icon(Icons.library_books_outlined),
+      //       selectedIcon: Icon(Icons.library_books),
+      //       label: 'Your Library',
+      //     ),
+      //     NavigationDestination(
+      //       icon: Icon(Icons.person_2_outlined),
+      //       selectedIcon: Icon(Icons.person_2),
+      //       label: 'Account',
+      //     ),
+      //   ],
+      // ),
     );
   }
 }
--- app/lib/app/features/discover/presentation/pages/discover_page.dart
+++ app/lib/app/features/discover/presentation/pages/discover_page.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:varanasi/app/shared/shared.dart';
 
 class DiscoverPage extends StatelessWidget {
   const DiscoverPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const CustomScrollView(
+      slivers: [
+        DashboardAppbar(),
+      ],
+    );
   }
 }
