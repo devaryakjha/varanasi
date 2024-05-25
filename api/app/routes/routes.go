d9be210f08dd8e87a35581e43e8852d6083e1216
aryakumar.jha <aryakumar.jha@zerodha.com>
2024-05-25 19:26:26 +0530

    chore: Add playlist details handler and service files

--- api/app/routes/routes.go
+++ api/app/routes/routes.go
@@ -3,9 +3,12 @@ package routes
 import (
 	"aryak.dev/varanasi/api/app/handlers"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/etag"
 )
 
 func SetupRoutes(app fiber.Router) {
-	app.Get("/discover", handlers.DiscoverHandler);
+	discoverRouter := app.Group("/discover")
+	discoverRouter.Use(etag.New())
+	discoverRouter.Get("", handlers.DiscoverHandler)
 	SetupMediaDetailsRoutes(app)
 }
--- api/server.go
+++ api/server.go
@@ -4,21 +4,19 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"time"
 
 	"aryak.dev/varanasi/api/app/routes"
 	"aryak.dev/varanasi/api/config"
 	"github.com/goccy/go-json"
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cache"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/spf13/viper"
 )
 
 func main() {
-    config.LoadConfig()
+	config.LoadConfig()
 
-    // first we must setup new Instance of FIber
+	// first we must setup new Instance of FIber
 	app := fiber.New(fiber.Config{
 		// ======== Start of JSON Config ========
 		JSONEncoder: json.Marshal,
@@ -26,26 +24,21 @@ func main() {
 		// ======== End of JSON Config ========
 	})
 
-    // Add logger middleware
+	// Add logger middleware
 	app.Use(logger.New())
 
-    // add caching middleware
-    isDebug := viper.GetBool("debug")
-    fmt.Println("debug: ", isDebug)
-    if !isDebug {
-        app.Use(cache.New(cache.Config{
-            Expiration: 24 * time.Hour,
-        }))
-    }
+	// add caching middleware
+	isDebug := viper.GetBool("debug")
+	fmt.Println("debug: ", isDebug)
 
 	app.Get("/", func(c *fiber.Ctx) error {
-    	return c.SendString("Hello, World!")
-    })
+		return c.SendString("Hello, World!")
+	})
 
 	v1Router := app.Group("/v1")
 	routes.SetupRoutes(v1Router)
 
-    // create http connection for this api
+	// create http connection for this api
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
